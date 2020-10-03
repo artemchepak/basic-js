4d4b430b8dbee0772eb40e14c78af32202fd976c
Artem Chepak <artemchepakwebdeveloper@users.noreply.github.com>
2020-10-03 12:07:43 +0300

    feat: add count-cats and dream-team functions

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,8 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(array/* matrix */) {
+  let result = 0;
+  array.forEach(secondArray =>
+    secondArray.forEach(element => (element === '^^') ? (result++) : result = result));
+  return result;
 };

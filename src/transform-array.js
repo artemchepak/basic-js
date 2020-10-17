a0f7c68362ff9deea2561cd83f1599aba329ea70
Artem Chepak <artemchepakwebdeveloper@users.noreply.github.com>
2020-10-18 00:37:17 +0300

    feat: add transform-array solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,60 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr){
+  let result = [];
+  let newArr = arr;
+
+  if(Object.prototype.toString.call(newArr) != '[object Array]'){
+      throw new Error('arr is not an Array');l
+  }
+  else{
+    for(i = 0; i < newArr.length; i++){ 
+      if(newArr[i] === '--discard-next'){
+        if(i === newArr.length - 1){
+          result = result;
+        }
+        else{
+          i++;
+        }
+      }
+      else if(newArr[i] === '--discard-prev'){
+        if(newArr[i - 2] === '--discard-next'){
+          result = result;
+        }
+        else{
+          if(i === 0){
+            result = result;
+          }
+          else{
+            result.pop(i - 1);
+          }
+        }
+      }
+      else if(newArr[i] === '--double-next'){
+        if(i === newArr.length - 1){
+          result = result
+        }
+        else{
+          result.push(newArr[i + 1]);
+        } 
+      }
+      else if(newArr[i]  === '--double-prev'){
+        if(newArr[i - 2] === '--discard-next'){
+          result = result;
+        }
+        else{
+          if(i === 0){
+            result = result;
+          }
+          else{
+            result.push(newArr[i - 1]); 
+          }
+        }
+      }
+      else{
+        result.push(newArr[i]); 
+      }
+    }
+  }  
+  return result;  
+}
